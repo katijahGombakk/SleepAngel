719367f0bab159ae333afd8a815e92657d8ae356
KatijahGombakk <41033883+katijahGombakk@users.noreply.github.com>
2018-07-10 11:50:55 +0800

    Add files via upload

---
+++ AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SleepAngell
+//
+//  Created by mo on 2018/7/10.
+//  Copyright © 2018年 Fan ge. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SleepAngell
+//
+//  Created by mo on 2018/7/10.
+//  Copyright © 2018年 Fan ge. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
